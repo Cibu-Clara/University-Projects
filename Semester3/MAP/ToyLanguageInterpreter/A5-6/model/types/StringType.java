ccc785b037a434c421190b4bf045923ad51dd2b6
Clara Cibu <125907393+Cibu-Clara@users.noreply.github.com>
2023-02-22 18:22:10 +0200

    Add files via upload

---
+++ Semester3/MAP/ToyLanguageInterpreter/A5-6/model/types/StringType.java
@@ -0,0 +1,20 @@
+package model.types;
+
+import model.values.StringValue;
+import model.values.Value;
+
+public class StringType implements Type{
+    @Override
+    public boolean equals(Type anotherType) { return anotherType instanceof StringType; }
+
+    @Override
+    public String toString() { return "string"; }
+
+    @Override
+    public Value defaultValue() { return new StringValue(""); }
+
+    @Override
+    public Type deepCopy() {
+        return new StringType();
+    }
+}
