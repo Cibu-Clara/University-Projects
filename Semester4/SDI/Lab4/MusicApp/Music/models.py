42dc098fb5c2a8536b057f7cf21d68c6295b78b0
911-Cibu-Clara <clara_cibu@yahoo.com>
2023-04-10 16:37:53 +0300

    sdi

---
+++ Semester4/SDI/Lab4/MusicApp/Music/models.py
@@ -0,0 +1,49 @@
+import datetime
+
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
+
+
+class Song(models.Model):
+    song_name = models.CharField(max_length=100)
+    composer = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    today = datetime.datetime.now()
+    year = today.year
+    year_of_release = models.PositiveSmallIntegerField(validators=[MinValueValidator(1800), MaxValueValidator(year)])
+
+    def __str__(self):
+        return self.song_name
+
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=100)
+    real_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.artist_name
+
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=100)
+    nr_of_tracks = models.IntegerField()
+    label = models.CharField(max_length=100)
+    today = datetime.datetime.now()
+    year = today.year
+    year_of_release = models.PositiveSmallIntegerField(validators=[MinValueValidator(1800), MaxValueValidator(year)])
+    main_artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
+
+    def __str__(self):
+        return self.album_title
+
+
+class PerformsOn(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.DO_NOTHING)
+    artist_id = models.ForeignKey(Artist, on_delete=models.DO_NOTHING)
+    nr_of_views = models.IntegerField()
+    duration = models.CharField(max_length=10, validators=[RegexValidator("..:..")])
+
+    def __str__(self):
+        return self.artist_id.artist_name + " - " + self.song_id.song_name
