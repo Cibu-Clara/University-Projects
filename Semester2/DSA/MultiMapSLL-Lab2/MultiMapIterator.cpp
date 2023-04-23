87b57b3abb560a15b54be395811a1ac6d3b92fd7
Clara Cibu <125907393+Cibu-Clara@users.noreply.github.com>
2023-04-23 18:36:18 +0300

    Update MultiMapIterator.cpp

--- Semester2/DSA/MultiMapSLL-Lab2/MultiMapIterator.cpp
+++ Semester2/DSA/MultiMapSLL-Lab2/MultiMapIterator.cpp
@@ -1,26 +1,33 @@
+#include <stdexcept>
 #include "MultiMapIterator.h"
 #include "MultiMap.h"
 
-
-MultiMapIterator::MultiMapIterator(const MultiMap& c): col(c) {
-	//TODO - Implementation
+//Complexity: Theta(1)
+MultiMapIterator::MultiMapIterator(const MultiMap& c) : col(c) {
+	this->current_node = c.head;
 }
 
-TElem MultiMapIterator::getCurrent() const{
-	//TODO - Implementation
-	return NULL_TELEM;
+//Complexity: Theta(1)
+TElem MultiMapIterator::getCurrent() const {
+	if (this->current_node == nullptr)
+		throw std::runtime_error("Iterator getCurrent error!");
+	TElem result = this->current_node->data;
+	return result;
 }
 
+//Complexity: Theta(1)
 bool MultiMapIterator::valid() const {
-	//TODO - Implementation
-	return false;
+	return this->current_node != nullptr;
 }
 
+//Complexity: Theta(1)
 void MultiMapIterator::next() {
-	//TODO - Implementation
+	if (this->current_node == nullptr)
+		throw std::runtime_error("Iterator next error!");
+	this->current_node = this->current_node->next;
 }
 
+//Complexity: Theta(1)
 void MultiMapIterator::first() {
-	//TODO - Implementation
+	this->current_node = this->col.head;
 }
-
