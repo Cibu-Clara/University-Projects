5fa2f735f1b58a3cbad98bceccf0f5340957b45b
Cibu-Clara <clara_cibu@yahoo.com>
2023-06-13 15:06:35 +0300

    lab2

---
+++ Semester4/DBMS/Lab2/Master_detail_windows_form/Master_detail_windows_form/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Master_detail_windows_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.refresh_button = new System.Windows.Forms.Button();
+            this.update_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(91, 50);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowTemplate.Height = 24;
+            this.dataGridViewParent.Size = new System.Drawing.Size(576, 192);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(91, 299);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(576, 202);
+            this.dataGridViewChild.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(327, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parent Table";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(327, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Child Table";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // refresh_button
+            // 
+            this.refresh_button.Location = new System.Drawing.Point(226, 528);
+            this.refresh_button.Name = "refresh_button";
+            this.refresh_button.Size = new System.Drawing.Size(119, 42);
+            this.refresh_button.TabIndex = 4;
+            this.refresh_button.Text = "REFRESH";
+            this.refresh_button.UseVisualStyleBackColor = true;
+            this.refresh_button.Click += new System.EventHandler(this.refresh_button_Click);
+            // 
+            // update_button
+            // 
+            this.update_button.Location = new System.Drawing.Point(378, 528);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(119, 42);
+            this.update_button.TabIndex = 5;
+            this.update_button.Text = "UPDATE";
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 591);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.refresh_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button refresh_button;
+        private System.Windows.Forms.Button update_button;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+
+        #endregion
+    }
+}
---
+++ Semester4/DBMS/Lab2/Master_detail_windows_form/Master_detail_windows_form/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Master_detail_windows_form
+{
+    public partial class Form1 : Form
+    {
+        private readonly DataSet _dataSet = new DataSet();
+        private SqlConnection _connection;
+
+        private SqlDataAdapter _childAdapter, _parentAdapter;
+        private readonly BindingSource _bindingSourceParent = new BindingSource();
+        private readonly BindingSource _bindingSourceChild = new BindingSource();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitDatabase();
+            InitUi();
+        }
+
+        private void InitDatabase()
+        {
+            String connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            String database = ConfigurationManager.AppSettings["Database"];
+
+            this._connection = new SqlConnection(String.Format(connectionString, database));
+            this._parentAdapter = new SqlDataAdapter(ConfigurationManager.AppSettings["SelectParent"], _connection);
+            this._childAdapter = new SqlDataAdapter(ConfigurationManager.AppSettings["SelectChild"], _connection);
+
+            new SqlCommandBuilder(this._childAdapter);
+            new SqlCommandBuilder(this._parentAdapter).GetInsertCommand();
+
+            this._parentAdapter.Fill(this._dataSet, ConfigurationManager.AppSettings["ParentTableName"]);
+            this._childAdapter.Fill(this._dataSet, ConfigurationManager.AppSettings["ChildTableName"]);
+
+            DataColumn parentDataColumn = this._dataSet.Tables[ConfigurationManager.AppSettings["ParentTableName"]].Columns[ConfigurationManager.AppSettings["ParentReferencedKey"]];
+            DataColumn childDataColumn = this._dataSet.Tables[ConfigurationManager.AppSettings["ChildTableName"]].Columns[ConfigurationManager.AppSettings["ChildForeignKey"]];
+
+            String relationName = ConfigurationManager.AppSettings["ForeignKey"];
+
+            var dataRelation = new DataRelation(
+                relationName,
+                parentDataColumn,
+                childDataColumn
+            );
+
+            this._dataSet.Relations.Add(dataRelation);
+        }
+
+        private void InitUi()
+        {
+            _bindingSourceParent.DataSource = _dataSet;
+            _bindingSourceParent.DataMember = ConfigurationManager.AppSettings["ParentTableName"];
+
+            _bindingSourceChild.DataSource = _bindingSourceParent;
+            _bindingSourceChild.DataMember = ConfigurationManager.AppSettings["ForeignKey"];
+
+            dataGridViewParent.DataSource = _bindingSourceParent;
+            dataGridViewChild.DataSource = _bindingSourceChild;
+        }
+
+        private void refresh_button_Click(object sender, EventArgs e)
+        {
+            _dataSet.Tables[ConfigurationManager.AppSettings["ChildTableName"]].Clear();
+            _dataSet.Tables[ConfigurationManager.AppSettings["ParentTableName"]].Clear();
+            
+            _parentAdapter.Fill(_dataSet, ConfigurationManager.AppSettings["ParentTableName"]);
+            _childAdapter.Fill(_dataSet, ConfigurationManager.AppSettings["ChildTableName"]);
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+            _childAdapter.Update(_dataSet, ConfigurationManager.AppSettings["ChildTableName"]);
+            _parentAdapter.Update(_dataSet, ConfigurationManager.AppSettings["ParentTableName"]);
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
