5ebede4490acaf0c8276f5449948dee2fa3b3c37
Cibu-Clara <clara_cibu@yahoo.com>
2024-03-06 11:39:41 +0200

    deleted lab7final

--- Semester5/FLCD/Lab7-final/DS/Node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, value):
-        self.father = -1
-        self.sibling = -1
-        self.value = value
-        self.production = -1
-
-    def __str__(self):
-        return str(self.value) + "  " + str(self.father) + "  " + str(self.sibling)
